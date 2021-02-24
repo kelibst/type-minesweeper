dd0974bd12cfdca0cfb835d537a7d38d57f125c6
Keli Booster <kbooster17@gmail.com>
2021-02-24 22:19:28 +0000

    start adding type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world!')
+document.addEventListener('DOMContentLoaded', ()=> {
+    const grid = document.querySelector('.grid');
+    let width: Number = 10
+    let bombAmount: Number = 20
+    let squares = []
+    let isGameOver: boolean = false;
+    let flags: Number = 0;
+    let matches: Number = 0;
+})
